7e08570196d65fe6c0e67f426b941f6128e2f699
Oncu <oohancan@gmail.com>
2023-03-04 21:42:33 +0300

    Initial Commit

---
+++ Transforming/TransformingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransformingApp.swift
+//  Transforming
+//
+//  Created by Oncu Can on 4.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct TransformingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
